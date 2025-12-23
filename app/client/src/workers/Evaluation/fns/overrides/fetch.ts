5f5d49bd2a4c95b1ea3448a2e6a594b111402acd
mozylee <mozylee@daou.co.kr>
2025-12-23 15:47:03 +0900

    feat: JSObject 코드 내 Fetch API에서 cookie 사용 가능하도록 수정 (1.0.26)

--- app/client/src/workers/Evaluation/fns/overrides/fetch.ts
+++ app/client/src/workers/Evaluation/fns/overrides/fetch.ts
@@ -2,8 +2,8 @@ const _originalFetch = self.fetch;
 
 export async function fetch(...args: Parameters<typeof _originalFetch>) {
   const request = new Request(args[0] as string, {
-    ...args[1],
-    credentials: "omit",
+    credentials: "omit", // 기본값: 보안 유지
+    ...args[1], // 사용자가 명시하면 덮어쓰기 허용
   });
 
   return _originalFetch(request);
